8c6f2c6497a8b9e3e400aae8fb8f62905bbfa486
Rodrigo-171 <rodrigonavarro171@gmail.com>
2020-08-24 21:27:06 -0300

    mudar conexao para banco online

--- php/conexao.php
+++ php/conexao.php
@@ -1,13 +1,17 @@
 <?php
 
 #ip do banco de dados do mysql
-define('HOST', '127.0.0.1:3307');
+// define('HOST', '127.0.0.1:3307');
+define('HOST', 'mysql.devs4teste.kinghost.net');
 #vai ter o usuário no banco de dados
-define('USUARIO', 'root');
+// define('USUARIO', 'root');
+define('USUARIO', 'devs4teste01');
 #vai armazenar a senha do banco de dados
-define('SENHA', '');
+// define('SENHA', '');
+define('SENHA', 'BFF9Yxz2aKq3mZp');
 #vai armazenar a base de dados
-define('DB', 'BDPET');
+// define('DB', 'BDPET');
+define('DB', 'devs4teste01');
 
 /*essa função vai receber as constantes na ordem. Se ele não conseguir conectar,
 ele vai dar um erro*/
