6fa06249fd50876da5f70febf438a2cca83ed48f
Rodrigo-171 <rodrigonavarro171@gmail.com>
2020-08-24 21:20:40 -0300

    mudar index para pegar url online

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <?php
     session_start(); // inicia a variavel $_SESSION
     $url = explode('/', $_SERVER['REQUEST_URI']); //pega a url e transforma em uma array
-    $page = $url[3]; // pega a que página a pessoa acessou, na sua maquina ai vai ser outro index provavelmente o index 2
+    // $page = $url[3]; // pega a que página a pessoa acessou, na sua maquina ai vai ser outro index provavelmente o index 2
+    $page = $url[1]; // pega a que página a pessoa acessou, na sua maquina ai vai ser outro index provavelmente o index 2
     var_dump($page);
     die;
 
